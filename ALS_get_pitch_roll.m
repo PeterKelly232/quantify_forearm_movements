8d391cb09884f5d2165c46a09cefc5a14a644385
MStraczkiewicz <56601451+MStraczkiewicz@users.noreply.github.com>
2024-08-05 09:19:38 +0200

    Update ALS_get_pitch_roll.m

--- ALS_get_pitch_roll.m
+++ ALS_get_pitch_roll.m
@@ -1,6 +1,6 @@
 function [pitch,roll] = ALS_get_pitch_roll(data_trimmed, df)
 % note negative sign in "roll formula"
-% formula valid for Actigraph Watch Link
+% formula valid for Actigraph Watch Link placed on the left wrist with a default placement and orientation (as in a smartwatch) 
 
 data_trimmed = data_trimmed(:, 1:3);
 
