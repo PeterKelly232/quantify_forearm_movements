e77af3bd865c69fc9fdd70d72eebdd5774076442
MStraczkiewicz <56601451+MStraczkiewicz@users.noreply.github.com>
2024-08-05 09:22:44 +0200

    Update ALS_get_pitch_roll.m

--- ALS_get_pitch_roll.m
+++ ALS_get_pitch_roll.m
@@ -1,6 +1,6 @@
 function [pitch,roll] = ALS_get_pitch_roll(data_trimmed, df)
-% note negative sign in "roll formula"
-% formula valid for Actigraph Watch Link placed on the left wrist with a default placement and orientation (as in a smartwatch) 
+% formula valid for Actigraph Watch Link placed on the left wrist
+% with a default placement and orientation (as in a smartwatch) 
 
 data_trimmed = data_trimmed(:, 1:3);
 
@@ -9,5 +9,5 @@ data_filtered = filtfilt(df, data_trimmed);
 data_filtered(data_filtered >  1) = 1;
 data_filtered(data_filtered < -1) = -1;
 
-pitch = atan2d(data_filtered(:, 2), sqrt(data_filtered(:, 1).^2 + data_filtered(:, 3).^2));
+pitch = atan2d(-data_filtered(:, 2), sqrt(data_filtered(:, 1).^2 + data_filtered(:, 3).^2));
 roll = atan2d(data_filtered(:, 1), data_filtered(:, 3));
